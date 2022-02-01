d2fa38a8ed46522f464f8f13d80f0fa6576a0099
nuelxelence <nuelxelence@gmail.com>
2022-02-01 20:04:49 +0100

    make it better

--- project/urls.py
+++ project/urls.py
@@ -1,9 +1,20 @@
+from unicodedata import name
 from django.urls import path
 from .views import *
 from . import views
 
 
 
+
+app_name = 'project'
 urlpatterns =[
-    path('',views.home, name='home')
+    path('',views.home, name='home'),
+    path('our_team/',views.team, name='our_team'),
+    path('property/',views.property, name='property'),
+    path('management/',views.management, name='management'),
+    path('marketing/',views.marketing, name='marketing'),
+    path('testimonial/',views.testimonial, name='testimonial'),
+    path('contactus/',views.contact_us, name='contactus')
+
+    
 ]
--- project/views.py
+++ project/views.py
@@ -5,3 +5,40 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+
+
+def team(request):
+    return render(request, 'team.html')
+
+
+
+
+def property(request):
+    return render(request, 'property.html')
+
+
+
+
+def management(request):
+    return render(request, 'management.html')
+
+
+
+
+
+def marketing(request):
+    return render(request, 'marketing.html')
+
+
+
+
+
+def testimonial(request):
+    return render(request, 'testimonial.html')
+
+
+
+
+def contact_us(request):
+    return render(request, 'contactus.html')
