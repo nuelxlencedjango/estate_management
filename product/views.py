4f0b210e7d9be4a7ded2d961656ee73b0d557571
nuelxelence <nuelxelence@gmail.com>
2022-04-11 12:33:00 +0100

    make it better

--- product/views.py
+++ product/views.py
@@ -83,10 +83,9 @@ def availableProperty(request):
 
             #pagination
             page_number = request.GET.get('page')
-            resultobj = paginator.get_page(page_number)
             paginator = Paginator(result, 10)
             
-            
+            resultobj = paginator.get_page(page_number)
 
             
 
@@ -98,7 +97,7 @@ def availableProperty(request):
             result= Property.objects.filter(price__range=(minpay, maxpay),name=name).order_by('-price')
             page_number = request.GET.get('page')
             resultobj = paginator.get_page(page_number)
-
+            
             paginator = Paginator(result, 10)
             
 
@@ -109,10 +108,9 @@ def availableProperty(request):
     else:
         resultobj =Property.objects.all().order_by('-price')
         page_number = request.GET.get('page')
-
-        resultobj = paginator.get_page(page_number)
         paginator = Paginator(result, 10)
             
+        resultobj = paginator.get_page(page_number)
         context ={
             'resultobj':resultobj
         }
