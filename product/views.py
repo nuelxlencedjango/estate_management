722e763f1282b59c63c3cae4c03927fe48db3a8a
nuelxelence <nuelxelence@gmail.com>
2022-04-18 16:22:27 +0100

    account setting

--- product/views.py
+++ product/views.py
@@ -395,6 +395,6 @@ def add_to_cart(request, pk):
 
     else:
         messages.info(request,"You need to register to be able to perform this operation")
-        return redirect('accounts:register') 
+        return redirect('accounts:login') 
         #messages.info(request,"Request unsuccessful! Please login before you can make a request")
         #return render(request ,'account/login.html') 
