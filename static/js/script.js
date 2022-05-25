a7e8e2300fa9a13659fbdb45a8a44f280e2edd73
nuelxelence <nuelxelence@gmail.com>
2022-05-25 12:11:49 +0100

    changes

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,30 @@
 
 
+let menuInfo = false;
+let main_menu = document.getElementById("menu");
+//let checkMenu = document.getElementById("want_buy");
+//let logo = document.getElementById("logo");
+
+function checkToggle(){
+if(menuInfo===false){
+
+    //checkMenu.style.marginTop ="380px";
+    //checkMenu.style.paddingTop ="360px";
+    main_menu.style.display = "block";
+    //main_menu.style.marginBottom="400px";
+    $("#menu").css({"margin-bottonr":"600px"});
+
+   
+    menuInfo =true;
+}
+else{
+    //checkMenu.style.marginTop ="30px";
+    main_menu.style.display = "none";
+    menuInfo =false;
+
+}
+}
+
 
 
 
