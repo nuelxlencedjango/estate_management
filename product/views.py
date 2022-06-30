301bda59f983e0e121ca226547cb9787b85a9a76
nuelxelence <nuelxelence@gmail.com>
2022-06-30 08:33:56 +0100

    image upload

--- product/views.py
+++ product/views.py
@@ -387,22 +387,13 @@ def allItems(request):
 
 
 
+
+
+
+
+
  
-
-
-def index(request):
-    user_list = User.objects.all()
-    page = request.GET.get('page', 1)
-
-    paginator = Paginator(user_list, 10)
-    try:
-        users = paginator.page(page)
-    except PageNotAnInteger:
-        users = paginator.page(1)
-    except EmptyPage:
-        users = paginator.page(paginator.num_pages)
-
-    return render(request, 'core/user_list.html', { 'users': users })   
+ 
 
 
 
