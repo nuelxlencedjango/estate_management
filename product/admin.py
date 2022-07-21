918302bfcd8854a203642821dd22011ccfa9a34e
nuelxelence <nuelxelence@gmail.com>
2022-07-21 07:29:21 +0100

    features

--- product/admin.py
+++ product/admin.py
@@ -56,18 +56,5 @@ admin.site.register(OurTeam)
 
 
 
-class FeaturedImageAdmin(admin.StackedInline):
-    model = FeaturedImages
-
-@admin.register(FeaturedListing)
-class  FeaturedListingAdmin(admin.ModelAdmin):
-    inlines = [FeaturedImageAdmin]
-
-    class Meta:
-       model = FeaturedListing
-
-@admin.register(FeaturedImages)
-class FeaturedImageAdmin(admin.ModelAdmin):
-    pass
 
 
