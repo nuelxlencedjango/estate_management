0a6edaa6d2701ccf30eaa58b2c69ab6c1f119f54
nuelxelence <nuelxelence@gmail.com>
2022-08-28 03:53:40 +0100

    contact form

--- product/views.py
+++ product/views.py
@@ -21,7 +21,7 @@ def home(request):
     def getIp(request):
         address = request.META.get('HTTP_X_FORWARDED_FOR')
         if address:
-            ip = address.split(';')[-1].strip()
+            ip = address.split(',')[-1].strip()
 
         else:
             ip = request.META.get('REMOTE_ADDR')
