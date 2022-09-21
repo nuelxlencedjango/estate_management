10dd4aef93fdca7998e585458fd7ebadb5dc5230
nuelxelence <nuelxelence@gmail.com>
2022-09-21 16:15:22 +0100

    email update

--- realestate/settings.py
+++ realestate/settings.py
@@ -31,7 +31,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY=os.environ.get('SECRET_KEY')
+SECRET_KEY='*d8ejn)%wq8l7ptet*ua6)+ak8p&4p&9&gz41yk-fafxj%a%_!'
+#os.environ.get('SECRET_KEY')
 
 
 
